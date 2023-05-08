a28a4b903fcc3798faf4e291fda0df3f2ab619af
realwanwanyi <realwanwanyi@gmail.com>
2023-05-08 16:39:53 +0800

    完成设置USERID的功能 设置gitignore

---
+++ configUserId.py
@@ -0,0 +1,30 @@
+import json
+
+file_path = "./config/userId.json"
+
+with open(file_path, "r") as file:
+    json_data = file.read()
+
+data = json.loads(json_data)
+
+for platform in data:
+    print("当前平台：" + platform["platform"])
+
+    input_list = platform["input_list"]
+
+    while True:
+        user_input = input("请输入用户ID（输入'q'退出）: ")
+
+        if user_input.lower() == "q":
+            break
+
+        input_data = {"userID": user_input}
+
+        input_list.append(input_data)
+
+    platform["input_list"] = input_list
+
+json_data = json.dumps(data, indent=4, ensure_ascii=False)
+
+with open(file_path, "w") as file:
+    file.write(json_data)
